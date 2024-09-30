4cacc97d62a545691367ed9aa9804c021389e450
Charalampos Mitrodimas <charmitro@posteo.net>
2024-09-30 14:26:58 +0200

    Add 'repo-branch' input
    Signed-off-by: Charalampos Mitrodimas <charmitro@posteo.net>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/sh -l
 
 REPO_URL=$1
-MANIFEST_PATH=$2
-IMAGE_TAG=$3
-USERNAME=$4
-EMAIL=$5
+REPO_BRANCH=$2
+MANIFEST_PATH=$3
+IMAGE_TAG=$4
+USERNAME=$5
+EMAIL=$6
 
 # Clone the ArgoCD manifests repository
 git config --global user.name "${USERNAME}"
 git config --global user.email "${EMAIL}"
-git clone "${REPO_URL}" manifests-repo
+git clone --branch "${REPO_BRANCH}" "${REPO_URL}" manifests-repo
 cd manifests-repo || exit 1
 
 # Replace the image tag
@@ -17,5 +18,5 @@ sed -i "s|newTag|${IMAGE_TAG}|g" "${MANIFEST_PATH}"
 
 # Commit and push the changes
 git commit -am "Update image tag to ${IMAGE_TAG}"
-git push
+git push origin "${REPO_BRANCH}"
 
